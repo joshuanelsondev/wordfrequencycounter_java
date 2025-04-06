daa94c4e40afbdc000e86e29dfe8bccb833f38bf
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-06 13:27:55 -0400

    feat: complete WordFrequencyCounter logic

--- src/main/java/joshuanelsondev/wordfrequencycounter/WordFrequencyCounter.java
+++ src/main/java/joshuanelsondev/wordfrequencycounter/WordFrequencyCounter.java
@@ -1,11 +1,46 @@
 package joshuanelsondev.wordfrequencycounter;
 
-public class WordFrequencyCounter {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class WordFrequencyCounter {
+
+    public static void main(String[] args) {
+        String wordFrequency = countWords("The rain in Spain falls mainly on the plain. The plain is quite dry.");
     }
 
     public static String countWords(String str) {
+
+        String normalizedText = normalizeString(str);
+
+        String[] words = normalizedText.split(" ");
+
+        HashMap<String, Integer> wordCounts = new HashMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                int wordCount = wordCounts.getOrDefault(word, 0);
+                wordCounts.put(word, ++wordCount);
+            }
+        }
+
+        String mostFrequentWord = null;
+        int maxWordFrequency = 0;
+
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            if (entry.getValue() > maxWordFrequency) {
+                mostFrequentWord = entry.getKey();
+                maxWordFrequency = entry.getValue();
+            }
+        }
+
+        if (mostFrequentWord != null) {
+            System.out.printf("Most used word: '%s'. Number of occurrences: %s", mostFrequentWord, maxWordFrequency);
+        } else {
+            System.out.println("No valid words found.");
+        }
+
+
+        return "";
 
     }
 
