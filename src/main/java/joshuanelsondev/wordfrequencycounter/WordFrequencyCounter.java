09d0dad5cf908777605ece19b96169586f8d5169
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-06 11:09:24 -0400

    Initial Commit

---
+++ src/main/java/joshuanelsondev/wordfrequencycounter/WordFrequencyCounter.java
@@ -0,0 +1,19 @@
+package joshuanelsondev.wordfrequencycounter;
+
+public class WordFrequencyCounter {
+    public static void main(String[] args) {
+
+    }
+
+    public static String countWords(String str) {
+
+    }
+
+    public static String normalizeString (String text) {
+        StringBuilder normalizedText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+        }
+
+    }
+}
